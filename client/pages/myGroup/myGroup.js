02bcfb46a6b3bc6fd581874c65852a29bc96dfbf
jmp-666 <jinminpeng0510@163.com>
2018-03-25 20:40:34 +0800

    change
    myGroup changed
    add myFriend and editFriend

--- client/pages/myGroup/myGroup.js
+++ client/pages/myGroup/myGroup.js
@@ -1,54 +1,39 @@
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 var util = require('../../utils/util.js')
+
+var app = getApp()
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
       userInfo:{},
-      listShow: [
-        { title: "1", content: "123", show: false }       
-      ],
+      listShow: {}
   },
-  creategroup: function () {
-    var newarray = [{
-      title: "name",
-      content: "123",
-      show: false
-    }];
-    this.setData({
-      'listShow': this.data.listShow.concat(newarray)
-    });
+
+  phoneNumTap: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['呼叫'],
+      success: function (res) {
+        if (res.tapIndex == 0) {
+          wx.makePhoneCall({
+            phoneNumber: that.data.phoneNum,
+          })
+        } 
+      }
+    })
   },
+
   showContent: function (e) {
     var index = parseInt(e.currentTarget.dataset.index);
     var key = "listShow[" + index + "].show";
     var val = this.data.listShow[index].show;
-    console.log(val);
     this.setData({
       [key]: !val
     })
-
-  },
-  remove: function (e) {
-
-    var dataset = e.target.dataset;
-    var Index = dataset.index; 
-
-    this.data.listShow.splice(Index, 1);
-
-    this.setData({
-      listShow: this.data.listShow
-    });
-  },
-  edit: function (e) {
-    var dataset = e.target.dataset;
-    var Index = dataset.index; 
-    this.data.list[Index].title = 'newname';
-    this.setData({
-      listShow: this.data.listShow
-    });
   },
 
   /**
@@ -63,54 +48,30 @@ Page({
         })
       }
     })
+    that.data.listShow = app.listShow,
+      that.setData({
+        'listShow': that.data.listShow
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          contentHeight: (res.windowHeight - 72 * res.screenWidth / 750)
+        });
+      }
+    }) 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onTabItemTap:function(){
+    wx.reLaunch({
+      url: '../index/index',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
